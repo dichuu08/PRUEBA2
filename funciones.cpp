37f2242cdcbea35d07a99f0d229ad9b0fc27d3b9
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-04-17 07:53:38 -0600

    Declaracion de variable identificador

---
+++ funciones.cpp
@@ -0,0 +1,12 @@
+/*Identificar la clasificación de un ángulo según su medida. Un ángulo puede ser: agudo si
+mide mas de 0° y menos de 90°; recto si mide 90°; obtuso si mide más de 90° y menos de
+180° y extendido si mide 180°*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+float angulo;
+string clasifAngulo(float angulo);
+
+
